45e595eaf3cee5cc343b2f63ef5b41f11764a50a
Mayank <mayankchaurasiya66@gmail.com>
2025-06-20 21:10:29 +0530

    updated UseState

--- Delta/React/React_js/react-project/src/App.jsx
+++ Delta/React/React_js/react-project/src/App.jsx
@@ -3,23 +3,26 @@ import ListItems from "./components/listitems";
 import ErrorMsg from "./components/ErrorMessag";
 import Container from "./components/Container";
 import FoodInput from "./components/FoodInput";
+import { useState } from "react";
 
 function App() {
   let foodItems = ["Fish", "Lentils", "curd", "Egg", "Roti/Rice"];
   // let foodItems = [];
 
-  let newlyAddedItems = "newly added";
+  let [currStateVal, setTextState] = useState("newly added items");
+
+  console.log(`current value of textState: ${currStateVal}`);
 
   const handleOnChange = (event) => {
     console.log(event.target.value);
-    newlyAddedItems = event.target.value;
+    setTextState(event.target.value);
   };
   return (
     <>
       <Container>
         <h1 className="heading">Healthy Foods</h1>
         <FoodInput handleOnChange={handleOnChange}></FoodInput>
-        <p>{newlyAddedItems}</p>
+        <p>{currStateVal}</p>
         <ErrorMsg foods={foodItems} />
         <ListItems foods={foodItems} />
       </Container>
