6192889fe55a14c764c54322f4ce6cd05466d06b
Mayank <mayankchaurasiya66@gmail.com>
2025-06-24 17:41:37 +0530

    updated todolist.jsx

--- Delta/React/Projects/ToDo-app/src/Components/TodoList.jsx
+++ Delta/React/Projects/ToDo-app/src/Components/TodoList.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
+
 export default function TodoList() {
+  let [todos, setTodos] = useState(["Sample task"]);
+  let [newTodo, setNewTodo] = useState("");
+
+  let addNewTask = () => {
+    setTodos([...todos, newTodo]);
+  };
+
+  let updateTaskVal = (event) => {
+    setNewTodo(event.target.value);
+  };
+
   return (
     <center>
       <h1>To Do App</h1>
       <div className="container">
-        <input type="text" placeholder="add a task" />
-        <button className="add-button">Add Task</button>
+        <input
+          type="text"
+          placeholder="add a task"
+          onChange={updateTaskVal}
+          value={newTodo}
+        />
+        <button className="add-button" onClick={addNewTask}>
+          Add Task
+        </button>
         <br />
         <br />
         <hr />
         <h4>Tasks Todo</h4>
-        <ul></ul>
+        <ul>
+          {todos.map((todo) => (
+            <li>{todo}</li>
+          ))}
+        </ul>
       </div>
     </center>
   );
