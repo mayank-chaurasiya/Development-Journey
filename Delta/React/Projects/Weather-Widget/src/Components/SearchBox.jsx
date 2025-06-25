b48e9a367d09027cac28d0e9cdb144f88848cbf5
Mayank <mayankchaurasiya66@gmail.com>
2025-06-25 16:28:55 +0530

    adding weather project

--- Delta/React/Projects/Weather-Widget/src/Components/SearchBox.jsx
+++ Delta/React/Projects/Weather-Widget/src/Components/SearchBox.jsx
@@ -1,7 +1,39 @@
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./SearchBox.css";
+import { useState } from "react";
+
 export default function SearchBox() {
+  let [city, setCity] = useState("");
+
+  let handleChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(city);
+    setCity("");
+  };
+
   return (
-    <div>
-      <input type="text" name="" id="" placeholder="Search for the Weather" />
+    <div className="SearchBox">
+      <h3>Search For the Weather</h3>
+      <form action="" onSubmit={handleSubmit}>
+        <TextField
+          id="outlined-required"
+          label="City Name"
+          variant="outlined"
+          required
+          size="small"
+          value={city}
+          onChange={handleChange}
+        />
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <Button variant="contained" type="submit">
+          Search
+        </Button>
+      </form>
     </div>
   );
 }
