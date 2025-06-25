af5d989b735dbd5ce3c56ce1f7aa823707186e64
Mayank <mayankchaurasiya66@gmail.com>
2025-06-25 15:24:16 +0530

    updated project folder

---
+++ Delta/React/MATERIALUi/mini-project-react/src/App.jsx
@@ -0,0 +1,13 @@
+// import "./App.css";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <>
+      <h1>Material UI Demo</h1>
+      <Button variant="contained">Click me!</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ Delta/React/Projects/Weather-Widget/src/Components/SearchBox.jsx
@@ -0,0 +1,7 @@
+export default function SearchBox() {
+  return (
+    <div>
+      <input type="text" name="" id="" placeholder="Search for the Weather" />
+    </div>
+  );
+}
