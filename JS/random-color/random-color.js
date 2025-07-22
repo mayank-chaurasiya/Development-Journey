9d5b388dd21a39c4f0cd7b196a944c49f4cf120c
Mayank <mayankchaurasiya66@gmail.com>
2025-07-22 21:34:30 +0530

    Added files for random-color

--- JS/random-color/random-color.js
+++ JS/random-color/random-color.js
@@ -1,5 +1,21 @@
 let button = document.querySelector("button");
 
 button.addEventListener("click", function () {
-  console.log("Generate random color");
+  let h3 = document.querySelector("h3");
+  let randomColor = getRandomColor();
+  h3.innerText = randomColor;
+
+  console.log("color updated");
+
+  let div = document.querySelector("div");
+  div.style.backgroundColor = randomColor;
 });
+
+function getRandomColor() {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+
+  let rgb = `rgb(${red}, ${green}, ${blue})`;
+  return rgb;
+}
