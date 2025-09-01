7ce58427c41589f644e7581e33793f1e54e0f58d
Mayank <mayankchaurasiya66@gmail.com>
2025-09-01 22:44:35 +0530

    updated files of sql class

--- SQL_CLASS/index.js
+++ SQL_CLASS/index.js
@@ -1,5 +1,10 @@
+const express = require("express");
 const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2");
+
+const app = express();
+
+let PORT = 8080;
 
 // Create the connection to database
 const connection = mysql.createConnection({
@@ -18,20 +23,40 @@ let createRandomUser = () => {
   ];
 };
 
-let query = "INSERT INTO user (id, username, email, password) VALUES ?";
+// ------ Generating - Inserting New Data --------------
 
-let data = [];
-for (let i = 1; i <= 100; i++) {
-  data.push(createRandomUser()); // 100 fake user's data
-}
+// let query = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let data = [];
+// for (let i = 1; i <= 100; i++) {
+//   data.push(createRandomUser()); // 100 fake user's data
+// }
 
-try {
-  connection.query(query, [data], (err, result) => {
-    if (err) throw err;
-    console.log(result);
-  });
-} catch (err) {
-  console.log(err);
-}
+// try {
+//   connection.query(query, [data], (err, result) => {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// } catch (err) {
+//   console.log(err);
+// }
+// connection.end();
 
-connection.end();
+// ----- HOME ROUTE --------------
+app.get("/", (req, res) => {
+  let total_users = `SELECT COUNT(*) FROM user`;
+
+  try {
+    connection.query(total_users, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send(result);
+    });
+  } catch (err) {
+    console.log(err);
+    res.send("Some error in our Database.");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`app is listenning to port ${PORT}`);
+});
