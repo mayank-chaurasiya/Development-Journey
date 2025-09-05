a988144e6b3210c55cf15c0700b010dfdcc49362
Mayank <mayankchaurasiya66@gmail.com>
2025-09-05 21:15:23 +0530

    Added files for mongodb

---
+++ mongoose/index.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+let url = "https://localhost:8080/users";
+
+main()
+  .then(() => {
+    console.log("connection successful!!");
+  })
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/office-data");
+}
+
+const employeeSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  salary: Number,
+});
+
+const Empdata = mongoose.model("Empdata", employeeSchema);
+
+const emp2 = new Empdata({
+  name: "Frederic",
+  email: "Frederic.Welch58",
+  salary: 85000,
+});
+
+emp2
+  .save()
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
