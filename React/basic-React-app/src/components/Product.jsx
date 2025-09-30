f7706a5d93ba7cdc75012523fa76c601d4777bcd
Mayank <mayankchaurasiya66@gmail.com>
2025-09-30 21:53:07 +0530

    updated react

--- React/basic-React-app/src/components/Product.jsx
+++ React/basic-React-app/src/components/Product.jsx
@@ -1,10 +1,22 @@
 import "./Product.css";
+import Price from "./Price";
 
-function Product(props) {
-  console.log(props.title);
-  return <div className="Product">
-    <p>Product</p>
-  </div>;
+function Product({ title, idx }) {
+  let oldPrices = ["12,879", "19,000", "1,00,000", "5,999"];
+  let newPrices = ["19,000", "12,879", "90,000", "3,999"];
+  let description = [
+    "silent clicks",
+    "designed for Ipad Pro",
+    "8k Display",
+    "3000 DPI",
+  ];
+  return (
+    <div className="Product">
+      <h4>{title}</h4>
+      <p>{description[idx]}</p>
+      <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]} />
+    </div>
+  );
 }
 
 export default Product;
--- React/basic-React-app/src/components/ProductCard.jsx
+++ React/basic-React-app/src/components/ProductCard.jsx
@@ -3,10 +3,10 @@ import "./ProductCard.css";
 function ProductCard() {
   return (
     <div className="card">
-      <Product />
-      <Product />
-      <Product />
-      <Product />
+      <Product title="Logitect mx Master" idx={0} />
+      <Product title="Apple Pencil" idx={1} />
+      <Product title="Samsung Laptop" idx={2} />
+      <Product title="Zebronics mouse M13" idx={3} />
     </div>
   );
 }
